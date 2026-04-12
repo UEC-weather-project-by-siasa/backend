72fcd69386430173b8f1a9429f0c7620c7f319c9
Siasa <siasa18037@gmail.com>
2026-04-12 18:24:46 +0700

    edit

--- src/modules/ai/ai.cron.js
+++ src/modules/ai/ai.cron.js
@@ -1,9 +1,9 @@
 const cron = require('node-cron');
 const { runBulkWeatherPredictionByAIModel } = require('./ai.service');
 
-// ตั้งเวลาให้ทำงานทุกๆ 15 นาที
+// ตั้งเวลาให้ทำงานทุกๆ 30 นาที
 const initAiCron = () => {
-  cron.schedule('*/15 * * * *', () => {
+  cron.schedule('*/30 * * * *', () => {
     console.log('Running Scheduled AI Weather Prediction...');
     // runBulkWeatherPredictionByAIModel();
   });
